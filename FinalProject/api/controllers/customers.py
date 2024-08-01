a67c38de98dc238596ca5fdb25deb4dd836047f8
DannySemakov <semakovdaniel@gmail.com>
2024-07-31 20:08:06 -0400

    feat: add customers module and edit credit_cards module in controllers package

--- FinalProject/api/controllers/customers.py
+++ FinalProject/api/controllers/customers.py
@@ -1,13 +1,15 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
-from ..models import credit_cards as model
+from ..models import customers as model
 from sqlalchemy.exc import SQLAlchemyError
 
 
 def create(db: Session, request):
-    new_item = model.CreditCard(
-        card_num  = request.card_num,
-        cvc = request.cvc
+    new_item = model.Customer(
+        name = request.name,
+        email = request.email,
+        phone_number = request.phone_num,
+        address = request.address
     )
 
     try:
@@ -23,7 +25,7 @@ def create(db: Session, request):
 
 def read_all(db: Session):
     try:
-        result = db.query(model.CreditCard).all()
+        result = db.query(model.Customer).all()
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
@@ -32,7 +34,7 @@ def read_all(db: Session):
 
 def read_one(db: Session, item_id):
     try:
-        item = db.query(model.CreditCard).filter(model.CreditCard.payment_id == item_id).first()
+        item = db.query(model.Customer).filter(model.Customer.id == item_id).first()
         if not item:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
     except SQLAlchemyError as e:
@@ -43,7 +45,7 @@ def read_one(db: Session, item_id):
 
 def update(db: Session, item_id, request):
     try:
-        item = db.query(model.CreditCard).filter(model.CreditCard.payment_id == item_id)
+        item = db.query(model.Customer).filter(model.Customer.id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         update_data = request.dict(exclude_unset=True)
@@ -57,7 +59,7 @@ def update(db: Session, item_id, request):
 
 def delete(db: Session, item_id):
     try:
-        item = db.query(model.CreditCard).filter(model.CreditCard.payment_id == item_id)
+        item = db.query(model.Customer).filter(model.Customer.id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         item.delete(synchronize_session=False)
