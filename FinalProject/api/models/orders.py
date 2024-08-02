fa846a8eda419d2a6be4562e623ef3d389919c21
DannySemakov <semakovdaniel@gmail.com>
2024-08-02 03:35:28 -0400

    feat: add comments for code clarification

--- FinalProject/api/models/orders.py
+++ FinalProject/api/models/orders.py
@@ -1,4 +1,3 @@
-"""
 from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, Double
 from sqlalchemy.orm import relationship
 from datetime import datetime
@@ -6,17 +5,14 @@ from ..dependencies.database import Base
 
 
 class Order(Base):
-   __tablename__ = "orders"
+    __tablename__ = "orders"
 
+    tracking_num = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    status = Column(String(50))
+    date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
+    price = Column(Double())
+    details_link = Column(String(200))
+    customer_id = Column(Integer, ForeignKey("customers.id"))
 
-   tracking_num = Column(Integer, primary_key=True, index=True, autoincrement=True)
-   status = Column(String(50))
-   date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
-   price = Column(Double(2))
-   details_link = Column(String(200))
-   customer_id = Column(Integer, ForeignKey("customers.id"))
-
-
-   customers = relationship("Customer", back_populates="orders")
-   orders_menu_items = relationship("OrderMenuItem", back_populates="orders")
-"""
+    customers = relationship("Customer", back_populates="orders")
+    #orders_menu_items = relationship("OrderMenuItem", back_populates="orders")
