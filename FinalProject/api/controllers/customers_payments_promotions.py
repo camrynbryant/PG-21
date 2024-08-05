650bbb4e5be1a159b1f94d9fa2374622ec54b64a
DannySemakov <semakovdaniel@gmail.com>
2024-08-05 18:57:53 -0400

    feat: add customers_payments_promotions controller module

--- FinalProject/api/controllers/customers_payments_promotions.py
+++ FinalProject/api/controllers/customers_payments_promotions.py
@@ -1,22 +1,3 @@
-####################################################
-#This module seems more complicated since it has one primary key made up of 3 attributes
-#
-#
-#
-#
-#
-##########################################################
-
-
-
-
-
-
-
-
-
-"""
-
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
 from ..models import customers_payments_promotions as model
@@ -24,17 +5,11 @@ from sqlalchemy.exc import SQLAlchemyError
 
 
 def create(db: Session, request):
-    new_item = model.CustomerPaymentPromotion()
-
-    try:
-        db.add(new_item)
-        db.commit()
-        db.refresh(new_item)
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
-
-    return new_item
+    new_item = model.CustomerPaymentPromotion(
+        customer_id = request.customers,
+        payment_id = request.payments,
+        promotion_id = request.promotions
+    )
 
 
 def read_all(db: Session):
@@ -46,9 +21,23 @@ def read_all(db: Session):
     return result
 
 
-def read_one(db: Session, item_id):
+def read_one(db: Session, item_ids):
+    """
+
+    :param db:
+    :param item_ids: an object that contains the 3 attributes which make up the primary key of the
+    customers_payments_promotions table. These 3 attributes include customer_id, payment_id, and promotion_id.
+    :return:
+    """
     try:
-        item = db.query(model.CustomerPaymentPromotion).filter(model.Customer.id == item_id).first()
+        customer_id = item_ids.customer_id
+        payment_id = item_ids.payment_id
+        promotion_id = item_ids.promotion_id
+
+        item = (db.query(model.CustomerPaymentPromotion).
+                filter(model.CustomerPaymentPromotion.customer_id == customer_id,
+                       model.CustomerPaymentPromotion.payment_id == payment_id,
+                       model.CustomerPaymentPromotion.promotion_id == promotion_id).first())
         if not item:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
     except SQLAlchemyError as e:
@@ -56,12 +45,19 @@ def read_one(db: Session, item_id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
     return item
 
-
-def update(db: Session, item_id, request):
+def update(db: Session, item_ids, request):
     try:
-        item = db.query(model.Customer).filter(model.Customer.id == item_id)
+        customer_id = item_ids.customer_id
+        payment_id = item_ids.payment_id
+        promotion_id = item_ids.promotion_id
+
+        item = (db.query(model.CustomerPaymentPromotion)
+                .filter(model.CustomerPaymentPromotion.customer_id == customer_id,
+                       model.CustomerPaymentPromotion.payment_id == payment_id,
+                       model.CustomerPaymentPromotion.promotion_id == promotion_id))
         if not item.first():
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="Customer_id, Payment_id, or Promotion_id not found!")
         update_data = request.dict(exclude_unset=True)
         item.update(update_data, synchronize_session=False)
         db.commit()
@@ -71,15 +67,28 @@ def update(db: Session, item_id, request):
     return item.first()
 
 
-def delete(db: Session, item_id):
+def delete(db: Session, item_ids):
     try:
-        item = db.query(model.Customer).filter(model.Customer.id == item_id)
+        customer_id = item_ids.customer_id
+        payment_id = item_ids.payment_id
+        promotion_id = item_ids.promotion_id
+
+        item = (db.query(model.CustomerPaymentPromotion)
+                .filter(model.CustomerPaymentPromotion.customer_id == customer_id,
+                       model.CustomerPaymentPromotion.payment_id == payment_id,
+                       model.CustomerPaymentPromotion.promotion_id == promotion_id))
         if not item.first():
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="Customer_id, Payment_id, or Promotion_id not found!")
         item.delete(synchronize_session=False)
         db.commit()
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-    """
+
+
+
+
+
+
