b470a2c404823b24174faa7b67af58ebf5ca50c7
DannySemakov <semakovdaniel@gmail.com>
2024-08-05 18:31:57 -0400

    fix: add customers controller

--- FinalProject/api/controllers/customers.py
+++ FinalProject/api/controllers/customers.py
@@ -1,4 +1,3 @@
-"""
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
 from ..models import customers as model
@@ -69,4 +68,3 @@ def delete(db: Session, item_id):
         error = str(e.__dict__['orig'])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-    """
