2cc6485d9d65b562c9c8f40b0961297360e86b1f
DannySemakov <semakovdaniel@gmail.com>
2024-08-05 18:35:11 -0400

    feat: add ingredients controller

--- FinalProject/api/controllers/ingredients.py
+++ FinalProject/api/controllers/ingredients.py
@@ -1,4 +1,3 @@
-"""
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
 from ..models import ingredients as model
@@ -68,4 +67,4 @@ def delete(db: Session, item_id):
         error = str(e.__dict__['orig'])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-    """
+
