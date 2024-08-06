00978ec598bda83e43d8eded35152030dcb903ae
DannySemakov <semakovdaniel@gmail.com>
2024-08-06 19:36:40 -0400

    fix: change menu_items_ingredients schema

--- FinalProject/api/schemas/menu_items_ingredients.py
+++ FinalProject/api/schemas/menu_items_ingredients.py
@@ -9,16 +9,19 @@ class MenuItemIngredientBase(BaseModel):
 class MenuItemIngredientCreate(MenuItemIngredientBase):
     menu_item_id: int
     ingredient_id: int
-    amount: int
 
 
-class MenuItemIngredientUpdate(MenuItemIngredientBase):
-    pass
+class MenuItemIngredientUpdate(BaseModel):
+    menu_item_id: int
+    ingredient_id: int
 
 
 class MenuItemIngredient(MenuItemIngredientBase):
     menu_item_id: int
     ingredient_id: int
 
-    class Config:
-        orm_mode = True
+    # IDK if the following is necessary, maybe already handled by inherited foreign key values
+    # menu_item: MenuItem
+    # ingredient: Ingredient
+    class ConfigDict:
+        from_attributes = True
