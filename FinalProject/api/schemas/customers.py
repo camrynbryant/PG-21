02886c574d4561276e4659de171f7b2c696db81d
DannySemakov <semakovdaniel@gmail.com>
2024-08-06 16:51:18 -0400

    fix: change customers schema

--- FinalProject/api/schemas/customers.py
+++ FinalProject/api/schemas/customers.py
@@ -10,8 +10,7 @@ class CustomerBase(BaseModel):
 
 
 class CustomerCreate(CustomerBase):
-    name: str
-    email: EmailStr
+    pass
 
 
 class CustomerUpdate(BaseModel):
@@ -21,44 +20,7 @@ class CustomerUpdate(BaseModel):
     address: Optional[str] = Field(None, max_length=100)
 
 
-class Review(BaseModel):
-    # Define the fields for the Review model here
-    id: int
-    review_text: str
-    rating: int
-    customer_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class Order(BaseModel):
-    # Define the fields for the Order model here
-    id: int
-    order_date: str
-    order_amount: float
-    customer_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class CustomerPaymentPromotion(BaseModel):
-    # Define the fields for the CustomerPaymentPromotion model here
-    id: int
-    payment_id: int
-    promotion_id: int
-    customer_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class Customer(CustomerBase):
     id: int
-    reviews: List[Review] = []
-    orders: List[Order] = []
-    customers_payments_promotions: List[CustomerPaymentPromotion] = []
-
-    class Config:
-        orm_mode = True
+    class ConfigDict:
+        from_attributes = True
