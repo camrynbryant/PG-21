55286abd28109d3425f4e3c5af38f07a277e9e15
DannySemakov <semakovdaniel@gmail.com>
2024-08-06 19:10:04 -0400

    fix: change orders schema

--- FinalProject/api/schemas/orders.py
+++ FinalProject/api/schemas/orders.py
@@ -3,10 +3,13 @@ from typing import Optional, List
 from datetime import datetime
 from decimal import Decimal
 
+from sqlalchemy import DECIMAL
+
+from FinalProject.api.models.customers import Customer
+
 
 class OrderBase(BaseModel):
     status: Optional[str] = Field(None, max_length=50)
-    date: Optional[datetime] = Field(default_factory=datetime.now)
     price: Optional[Decimal]
     details_link: Optional[str] = Field(None, max_length=200)
 
@@ -15,23 +18,16 @@ class OrderCreate(OrderBase):
     customer_id: int
 
 
-class OrderUpdate(OrderBase):
-    pass
-
-
-class OrderMenuItem(BaseModel):
-    id: int
-    order_id: int
-    menu_item_id: int
-
-    class Config:
-        orm_mode = True
+class OrderUpdate(BaseModel):
+    status: Optional[str] = None
+    price: Optional[DECIMAL] = None
+    details_link: Optional[str] = None
+    customer_id: Optional[int] = None
 
 
 class Order(OrderBase):
     tracking_num: int
-    customer_id: int
-    orders_menu_items: List[OrderMenuItem] = []
+    customer: Customer
 
-    class Config:
-        orm_mode = True
+    class ConfigDict:
+        from_attributes = True
