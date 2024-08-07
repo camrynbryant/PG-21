a1cfc73a2c151231725fc0788ab1264c4ca3c277
DannySemakov <semakovdaniel@gmail.com>
2024-08-07 02:01:49 -0400

    fix: remove unncessary comment

--- FinalProject/api/routers/index.py
+++ FinalProject/api/routers/index.py
@@ -1,9 +1,5 @@
 from . import customers, orders, payments, promotions, reviews, ingredients, credit_cards, menu_items, \
-    customers_payments_promotions, orders_menu_items, menu_items_ingredients
-
-
-#cd C:/Users/andmu/PycharmProjects/PG-21
-#py -m uvicorn FinalProject.api.main:app --reload
+    customers_payments_promotions, orders_menu_items, menu_items_ingr
 def load_routes(app):
     app.include_router(customers.router)
     app.include_router(orders.router)
